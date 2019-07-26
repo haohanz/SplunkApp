d6c7b2be37cb23c95cb64bf3fa339ba00696af26
haohanz <hh1680651@gmail.com>
2019-07-26 00:57:44 -0400

    fix python script format and .travis.yml

--- code/Test/Python/node.py
+++ code/Test/Python/node.py
@@ -6,6 +6,11 @@ import time
 import threading
 import json
 
+'''
+Performance test for Node server.
+Usage: python3 node.py <concurrent_thread_number>
+'''
+
 baseurl = 'http://localhost:4000/cve_search'
 query = 'CVE-2019-9978'
 concurrency = int(sys.argv[1])
@@ -13,18 +18,19 @@ concurrency = int(sys.argv[1])
 durations = []
 
 def search():
-	now = time.time()
-	res = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/','POST', body=json.dumps({'cve': query}))[1]
-	duration = time.time() - now
-	durations.append(duration)
-	print(duration)
+    now = time.time()
+    res = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/','POST', body=json.dumps({'cve': query}))[1]
+    duration = time.time() - now
+    durations.append(duration)
+    print('duration: %f ms' % (1000 * duration))
 
 threads = []
 for i in range(concurrency):
-	threads.append(threading.Thread(target = search))
-	threads[-1].start()
+    threads.append(threading.Thread(target = search))
+    threads[-1].start()
 
 for i in range(concurrency):
-	threads[i].join()
+    threads[i].join()
 
-print(sum(durations) / len(durations))
+print('avg duration: %f ms' % (1000 * sum(durations) / len(durations)))
+
--- code/Test/Python/splunkapi.py
+++ code/Test/Python/splunkapi.py
@@ -5,22 +5,31 @@ import sys
 import time
 import threading
 
+'''
+Performance test for SafeMed Splunk application's search function using Splunk RESTful API.
+Usage: python3 splunkapi.py <query_index> <concurrent_thread_number>
+'''
+
 baseurl = 'https://localhost:8089'
 userName = 'admin'
 password = 'password'
-queries = ['index="hospital_data" | rename "IP Address 1" as HOST | join HOST type=inner [search index="nexpose" | rename "extracted_Host" as HOST] | join cve type=outer [search index=cve ] | join cwe type=outer [search index=cwe cwe=*] | fields "Manufacturer", "Model", "cwe", "vuln_technical_details_0_link"',
- '"Manufacturer_3" index="hospital_data"        | rename "IP Address 1" as HOST        | join HOST type=inner        [search index="nexpose" | rename "extracted_Host" as HOST]        | join cve type=outer [search index=cve ]            | join cwe type=outer [search index=cwe cwe=* ]',
- '"Manufacturer_3" "Model_12" index="hospital_data"        | rename "IP Address 1" as HOST        | join HOST type=inner        [search index="nexpose" | rename "extracted_Host" as HOST]        | join cve type=outer [search index=cve ]            | join cwe type=outer [search index=cwe cwe=* ]',
- '"Manufacturer_3" "Model_12" index="hospital_data" | rename "IP Address 1" as HOST | join HOST type=inner [search index="nexpose" | rename "extracted_Host" as HOST] | join cve type=inner [search index=cve cwe=* "CWE-78"]',
- 'index=nexpose | head 100',
- 'index="hospital_data"']
+queries = [
+        'index="hospital_data" | rename "IP Address 1" as HOST | join HOST type=inner [search index="nexpose" | rename "extracted_Host" as HOST] | join cve type=outer [search index=cve ] | join cwe type=outer [search index=cwe cwe=*] | fields "Manufacturer", "Model", "cwe", "vuln_technical_details_0_link"',
+        '"Manufacturer_3" index="hospital_data" | rename "IP Address 1" as HOST | join HOST type=inner [search index="nexpose" | rename "extracted_Host" as HOST] | join cve type=outer [search index=cve ] | join cwe type=outer [search index=cwe cwe=* ]',
+        '"Manufacturer_3" "Model_12" index="hospital_data" | rename "IP Address 1" as HOST | join HOST type=inner [search index="nexpose" | rename "extracted_Host" as HOST] | join cve type=outer [search index=cve ] | join cwe type=outer [search index=cwe cwe=* ]',
+        '"Manufacturer_3" "Model_12" index="hospital_data" | rename "IP Address 1" as HOST | join HOST type=inner [search index="nexpose" | rename "extracted_Host" as HOST] | join cve type=inner [search index=cve cwe=* "CWE-78"]',
+        'index=nexpose | head 100',
+        'index="hospital_data"']
 searchQuery = queries[int(sys.argv[1])]
 concurrency = int(sys.argv[2])
 
 # Authenticate with server.
 # Disable SSL cert validation. Splunk certs are self-signed.
-serverContent = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/auth/login',
-    'POST', headers={}, body=urllib.parse.urlencode({'username':userName, 'password':password}))[1]
+serverContent = httplib2.Http(disable_ssl_certificate_validation=True).request(
+        baseurl + '/services/auth/login',
+        'POST',
+        headers={},
+        body=urllib.parse.urlencode({'username':userName, 'password':password}))[1]
 
 sessionKey = minidom.parseString(serverContent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
 
@@ -36,26 +45,26 @@ print(searchQuery)
 durations = []
 
 # Run the search.
-# Again, disable SSL cert validation. 
+# Again, disable SSL cert validation.
 def search():
-	sid = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs','POST',
-	    headers={'Authorization': 'Splunk %s' % sessionKey},body=urllib.parse.urlencode({'search': searchQuery}))[1]
-	sid = sid.split(b'sid')[1][1: -2]
-	print(sid)
-	time.sleep(10)
-	job_detail = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/' + sid.decode(),'GET',
-	    headers={'Authorization': 'Splunk %s' % sessionKey})[1].decode()
-	duration = job_detail[job_detail.find('runDuration') + 13: ]
-	duration = duration[: duration.find('\n') - 9]
-	durations.append(float(duration))
-	print(duration + 's')
+    sid = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs','POST',
+        headers={'Authorization': 'Splunk %s' % sessionKey},body=urllib.parse.urlencode({'search': searchQuery}))[1]
+    sid = sid.split(b'sid')[1][1: -2]
+    time.sleep(10)
+    job_detail = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/' + sid.decode(),'GET',
+        headers={'Authorization': 'Splunk %s' % sessionKey})[1].decode()
+    duration = job_detail[job_detail.find('runDuration') + 13: ]
+    duration = duration[: duration.find('\n') - 9]
+    durations.append(float(duration))
+    print('duration: %f ms' % (1000 * float(duration)))
 
 threads = []
 for i in range(concurrency):
-	threads.append(threading.Thread(target = search))
-	threads[-1].start()
+    threads.append(threading.Thread(target = search))
+    threads[-1].start()
 
 for i in range(concurrency):
-	threads[i].join()
+    threads[i].join()
 
-print(sum(durations) / len(durations))
+print('avg duration: %f ms' % (1000 * sum(durations) / len(durations)))
+
